8be3ef3d99f270270308bcb5c255cc543ba7c9e7
HyeockJinKim <kherootz@gmail.com>
2020-02-01 14:29:34 +0900

    Initial commit

---
+++ src/main/kotlin/com/LAMP/LAMPBackend/LampBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.LAMP.LAMPBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LampBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<LampBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/LAMP/LAMPBackend/LampBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.LAMP.LAMPBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LampBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
